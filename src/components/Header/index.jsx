77ef36750e9c7f5be84bf7c0a88fdfa035c16c11
PogodiPostoi <boombox577@gmail.com>
2022-07-21 20:51:23 +0300

    header info finish

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -9,12 +9,27 @@ function Header() {
             </div>
             <div className={styles.headerInfo}>
                 <div className={styles.headerInfoLogo}>
-                    <img src="/img/catalog/logo.png" alt="logo" />
+                    <img src="/img/header/logo.png" alt="logo" />
                     <span>winestory</span>
                 </div>
-                <div>search</div>
-                <div>phone</div>
-                <div>currency</div>
+                <div className={styles.headerInfoSearch}>
+                    <input placeholder="search"/>
+                    <img src="/img/arrow.png" alt="arrow" 
+                        width={20} 
+                        height={20}
+                    />
+                </div>
+                <div className={styles.headerInfoPhone}>
+                    <a href="tel:+76660852969">(666)085 29 69</a>
+                </div>
+                <div className={styles.headerInfoCurrency}>
+                    <select name="#" id="#">
+                        <option value="1">rur</option>
+                        <option value="2">usd</option>
+                        <option value="1">eur</option>
+                        <option value="1">uah</option>
+                    </select>
+                </div>
                 <div>account</div>
                 <div>cart</div>
             </div>
