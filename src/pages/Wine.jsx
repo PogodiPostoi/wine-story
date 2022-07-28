c7092d7801edaf55a427e3fa3659743ceefd37d8
PogodiPostoi <boombox577@gmail.com>
2022-07-28 22:01:56 +0300

    starting layout wine page

--- src/pages/Wine.jsx
+++ src/pages/Wine.jsx
@@ -9,8 +9,18 @@ function Wine({pageName}) {
         <>
             <h1 className="pageName">{pageName}</h1>
             <div className="wine">
+                <aside>
+                    {/* TODO: Фильтр вынести в отдельный компонент */}
+                    <h3>Filter</h3>
+                    <div className="priceFilter">
+                        <p>price</p>
+                        <p>+</p>
+                    </div>
+                    
+                </aside>
                 <div className="wineItems">
                     {/* <CatalogCard /> */}
+                    1
                 </div>
             </div>
         </>
